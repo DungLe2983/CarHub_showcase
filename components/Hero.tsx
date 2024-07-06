05522928263641d809b4d9b26fdcf1c8bfce937a
DungLe2983 <lequocdung2983@gmail.com>
2024-07-06 20:24:46 +0700

    chore: add demo image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,10 @@
-'use client'
+'use client';
 import React from 'react';
 import Image from 'next/image';
 import CustomButton from './CustomButton';
 
 const Hero = () => {
-  const handleScroll = () =>{
-
-  }
+  const handleScroll = () => {};
   return (
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
